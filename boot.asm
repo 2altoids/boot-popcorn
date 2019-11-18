db32cb8c146fa350bfbb6ec8187964e2946d27fb
Michael S <altoids3@mail.com>
2019-11-17 19:02:39 -0700

    Add BIOS parameter block & print routine
    -Add BIOS parameter block containing disk metadata
    -Add print routine using BIOS 10H interrupt
    -Halt program using hlt instead of jmp infinite loop

--- boot.asm
+++ boot.asm
@@ -6,19 +6,62 @@ org 0x7c00              ; The BIOS loads the boot sector at 0x7c00
 
 bits 16                 ; The bootloader begins in 16 bit real mode
 
-start:
-    mov ah, 0x0e        ; Teletype output function code for INT 0x10
-    mov al, 'H'
-    int 0x10            ; Execute video interrupt 0x10
-    mov al, 'E'
-    int 0x10
-    mov al, 'L'
-    int 0x10
-    int 0x10
-    mov al, 'O'
-    int 0x10
 
-    jmp $               ; Jump to current address.  Infinite loop.
+start:
+    jmp loader
+
+
+
+bpbName                 db "PopcornB"
+
+;***********BIOS Partition Table***********;
+; This 
+bpbBytesPerSector:  	dw 512
+bpbSectorsPerCluster: 	db 1
+bpbReservedSectors: 	dw 1
+bpbNumberOfFATs: 	    db 2
+bpbRootEntries: 	    dw 224
+bpbTotalSectors: 	    dw 2880
+bpbMedia: 	            db 0xf0
+bpbSectorsPerFAT: 	    dw 9
+bpbSectorsPerTrack: 	dw 18
+bpbHeadsPerCylinder: 	dw 2
+bpbHiddenSectors: 	    DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber: 	        db 0
+bsUnused: 	            db 0
+bsExtBootSignature: 	db 0x29
+bsSerialNumber:	        dd 0xa0a1a2a3
+bsVolumeLabel: 	        db "MOS FLOPPY "
+bsFileSystem: 	        db "FAT12   "
+;******************************************;
+
+
+msg:                    db "Executing Stage 1...", 0
+
+
+;Print routine
+print:
+    lodsb               ; Load next byte in string
+    or al, al           ; If byte is 0, return, else print character.
+    jz printDone
+    mov ah, 0x0e
+    int 0x10
+    jmp print
+printDone:
+    ret
+
+
+loader:
+    xor ax, ax          ; Null segment registers, no offsets required
+    mov ds, ax
+    mov es, ax
+    
+    mov si, msg         ; Print message string at msg
+    call print
+
+    cli                 ; Clear all interrupts
+    hlt                 ; Halt the system
 
 times 510 - ($-$$) db 0 ; Pad boot sector with 0's
 
