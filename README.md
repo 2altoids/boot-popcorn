6e88bebb3721692e7bbe3b56205b47f45c5ca943
Michael S <altoids3@mail.com>
2019-12-04 13:47:28 -0700

    Add sector loading from boot sector in boot.bin
    -Make boot sector load second sector written in same file
    -Jump to 2nd sector in RAM using Global Descriptor Table
    -Run second sector in 32 bit protected mode
    -Write text example to VGA buffer

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This will use NASM to assemble `boot.asm` into a binary executable called `boot.
 ## Run instructions
 Running the binary requires QEMU.  On your system you may be able to use either the command `qemu` or `qemu-system-x86_64`.
 
-To boot from the binary, run `qemu-system-x86_64 boot.bin`
+To boot from the binary, run `qemu-system-x86_64 -fda boot.bin`
 
 ---
 
--- boot.asm
+++ boot.asm
@@ -8,69 +8,131 @@ bits 16                 ; The bootloader begins in 16 bit real mode
 
 
 start:
-    jmp loader
+    jmp main
 
+;***********GLOBAL DESCRIPTOR TABLE********;
+gdt_start:
+    dq 0x0          ; null descriptor (8 zeroed bytes)
+gdt_code:
+    dw 0xFFFF       ; limit low
+    dw 0x0          ; base low
+    db 0x0          ; base middle
+    db 10011010b    ; access byte
+    db 11001111b    ; granularity byte
+    db 0x0          ; base high
+gdt_data:
+    dw 0xFFFF       ; limit low
+    dw 0x0          ; base low
+    db 0x0          ; base middle
+    db 10010010b    ; access
+    db 11001111b    ; granularity
+    db 0x0          ;base high
+gdt_end:
 
-
-TIMES 0x0b - $ + start db 0
-
-;***********BIOS Partition Table***********;
-; This 
-bpbBytesPerSector:  	dw 512
-bpbSectorsPerCluster: 	db 1
-bpbReservedSectors: 	dw 1
-bpbNumberOfFATs: 	    db 2
-bpbRootEntries: 	    dw 224
-bpbTotalSectors: 	    dw 2880
-bpbMedia: 	            db 0xf0
-bpbSectorsPerFAT: 	    dw 9
-bpbSectorsPerTrack: 	dw 18
-bpbHeadsPerCylinder: 	dw 2
-bpbHiddenSectors: 	    DD 0
-bpbTotalSectorsBig:     DD 0
-bsDriveNumber: 	        db 0
-bsUnused: 	            db 0
-bsExtBootSignature: 	db 0x29
-bsSerialNumber:	        dd 0xa0a1a2a3
-bsVolumeLabel: 	        db "POPCORNBOOT"
-bsFileSystem: 	        db "FAT12   "
+gdt_pointer:
+    dw gdt_end - gdt_start
+    dd gdt_start
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 ;******************************************;
 
 
-msg:                    db "Boot sector:  Executing Stage 1...", 0
+msg:                db "Boot sector:  Executing Stage 1...", 13, 10, 0
+disk_error_msg:     db "Disk read error", 0
+sectors_error_msg:  db "Incorrect number of sectors read", 0
+jumping_msg:        db "Boot sector:  Loaded new sector(s) into memory.", 13, 10, "Jumping to kernel entrypoint...", 13, 10, 0
+disk: db 0x00
 
 
 ;Print routine
 print:
     lodsb               ; Load next byte in string
     or al, al           ; If byte is 0, return, else print character.
-    jz printDone
+    jz print_done
     mov ah, 0x0e
     int 0x10
     jmp print
-printDone:
+print_done:
     ret
-
-
-loader:
-    xor ax, ax          ; Null segment registers, no offsets required
-    mov ds, ax
-    mov es, ax
     
+    
+disk_error:
+    mov si, disk_error_msg
+    call print
+    jmp $
+
+
+sectors_error:
+    mov si, sectors_error_msg
+    call print
+    jmp $
+
+
+main:
+
+    mov ax, 0x2401      ; Activate A20 line
+	int 0x15
+	
+    mov ax, 0x3         ; Set VGA mode to safe value (text mode 3)
+	int 0x10
+    
+    mov bp, 0x8000      ; Move the stack far away
+    mov sp, bp
+    
+    ;Print loading message
     mov si, msg         ; Print message string at msg
     call print
-
-    cli                 ; Clear all interrupts
-    hlt                 ; Halt the system
+    
+    ;Load next stage into RAM         
+    mov [disk],dl       
+    mov ah, 0x2         ;read sectors
+    mov al, 1           ;number of sectors to read
+    mov ch, 0           ;cylinder idx
+    mov dh, 0           ;head idx
+    mov cl, 2           ;sector idx
+    mov dl, [disk]      ;disk idx
+    mov bx, copy_target
+    int 0x13
+    
+    ;Set up Global Descriptor Table
+    cli                 ; Clear interrupts
+    lgdt [gdt_pointer]  ; load the gdt table
+    mov eax, cr0 
+    or eax,0x1          ; set the protected mode bit on special CPU reg cr0
+    mov cr0, eax
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	
+    
+    jmp CODE_SEG:kernel
 
 times 510 - ($-$$) db 0 ; Pad boot sector with 0's
 
 dw 0xaa55               ; Boot signature / bootable "magic number"
                         ; 0x55 and 0xAA. Note little-endian ordering.
-                        
-                        
-                        
-                        
-                        
-                        
 
+;==========================SEGMENT 2==============================
+copy_target:
+bits 32
+
+hello: db "Hello more than 512 bytes world!!",0
+kernel:
+	mov esi,hello
+	mov ebx,0xb8000
+.loop:
+	lodsb
+	or al,al
+	jz halt
+	or eax,0x0F00
+	mov word [ebx], ax
+	add ebx,2
+	jmp .loop
+halt:
+	cli
+	hlt
+
+times 1024 - ($-$$) db 0
